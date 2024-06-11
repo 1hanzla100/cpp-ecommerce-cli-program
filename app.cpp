93019beb2292f3007e3fa2a50d80b2f109e1e98d
1hanzla100 <hanzla.tauqeer123@gmail.com>
2024-06-11 11:59:05 +0500

    Added Cart Func

--- app.cpp
+++ app.cpp
@@ -18,6 +18,12 @@ struct Product
     int purchases;
 };
 
+struct Cart
+{
+    Product product;
+    int quantity;
+};
+
 void displayMainMenu()
 {
     cout << "------ Main Menu ------" << endl;
@@ -36,7 +42,7 @@ void displayMainMenu()
 
 void displayProducts(Product products[], int productCount)
 {
-    for (int i = 0; i < productCount; i++)
+    for (int i = 1; i < productCount; i++)
     {
         cout << "ID: " << i << endl;
         cout << "Name: " << products[i].name << endl;
@@ -47,29 +53,124 @@ void displayProducts(Product products[], int productCount)
     }
 }
 
+void searchProducts(Product products[], int productCount, string input)
+{
+    for (int i = 0; i < productCount; i++)
+    {
+        if (input == products[i].category || input == products[i].name)
+        {
+            cout << i << " \t " << products[i].name << " \t " << products[i].price << " \t " << products[i].brand << endl;
+        }
+    }
+}
+
+void displayDetails(Product products[], int productCount, int index)
+{
+    for (int i = 0; i < productCount; i++)
+    {
+        if (index == i)
+        {
+            cout << "Name:  " << products[i].name << endl;
+            cout << "Price:  " << products[i].price << endl;
+            cout << "Brand:  " << products[i].brand << endl;
+            cout << "Description: " << products[i].description << endl;
+            cout << "Purchases: " << products[i].purchases << endl;
+            cout << "Stock: " << products[i].stock << endl;
+        }
+    }
+}
+Cart addToCart(Product products[], int productCount)
+{
+    int productId;
+    int quantity;
+
+    while (true)
+    {
+        cout << "Enter the product ID: ";
+        cin >> productId;
+        if (productId < 0 || productId >= productCount)
+        {
+            cout << "Invalid product ID. Please enter a valid product ID: ";
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        cout << "Enter the quantity: ";
+        cin >> quantity;
+        if (quantity < 0 || quantity > products[productId].stock)
+        {
+            cout << "Invalid quantity. Please enter a valid quantity: ";
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    Cart cart;
+    cart.product = products[productId];
+    cart.quantity = quantity;
+    return cart;
+}
+
+void displayCart(Cart cart[], int cartCount)
+{
+    for (int i = 0; i < cartCount; i++)
+    {
+        cout << "Item #" << i + 1 << endl;
+        cout << "Product: " << cart[i].product.name << endl;
+        cout << "Price: " << cart[i].product.price << endl;
+        cout << "Quantity: " << cart[i].quantity << endl;
+        cout << "Subtotal: " << cart[i].product.price * cart[i].quantity << endl;
+        cout << endl;
+    }
+}
+
+void removeItemFromCart(Cart *cart, int *cartCount)
+{
+    int index;
+    cout << "Enter the item number to remove: ";
+    cin >> index;
+    if (index == 0 || index >= *cartCount)
+    {
+        cout << "Invalid item number. Please enter a valid item number." << endl;
+    }
+    else
+    {
+        if (index - 1 == *cartCount - 1 || index - 1 == 0)
+        {
+            (*cartCount)--;
+            return;
+        }
+        for (int i = index; i < *cartCount - 1; i++)
+        {
+            *(cart + i) = *(cart + i + 1);
+        }
+        (*cartCount)--;
+    }
+}
+
 int main()
 {
     // Read CSV with these attributes
 
     ifstream productFileIn("Products.csv");
-    if (!productFileIn.is_open())
-    {
-        cout << "Error opening file" << endl;
-        return 1;
-    }
 
     Product products[MAX_RECORDS];
     int productCount = 0;
 
-    // Loading the products from the file
+    Cart cart[MAX_RECORDS];
+    int cartCount = 0;
 
     string line;
     istringstream ss;
     while (getline(productFileIn, line))
     {
         ss = istringstream(line);
-        // read the attributes and store them in the struct array
-
         getline(ss, products[productCount].name, ',');
         ss >> products[productCount].price;
         ss.ignore();
@@ -98,6 +199,34 @@ int main()
         {
         case 1:
             displayProducts(products, productCount);
+            break;
+        case 2:
+        {
+            string input;
+            cout << "Enter which category you want to buy:";
+            cin >> input;
+            searchProducts(products, productCount, input);
+        }
+        break;
+        case 3:
+        {
+            int index;
+            cout << "Enter the product for Details:";
+            cin >> index;
+            displayDetails(products, productCount, index);
+        }
+        break;
+        case 4:
+        {
+            cart[cartCount] = addToCart(products, productCount);
+            cartCount++;
+        }
+        break;
+        case 5:
+            displayCart(cart, cartCount);
+            break;
+        case 6:
+            removeItemFromCart(cart, &cartCount);
             break;
         default:
             exit = true;
